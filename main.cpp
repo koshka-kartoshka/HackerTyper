7c9d1df55a4db4d56cf47f17132e27bb4d90629a
koshka-kartoshka <71890099+koshka-kartoshka@users.noreply.github.com>
2020-12-12 22:50:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 #include <iostream>
 #include <cstdio>
 #include <stdio.h>
-#include <cassert>
-#include <string.h>
+#include "String.hpp"
+
+
+int fileLen(FILE* file)
+{
+    fseek(file, 0, SEEK_END);
+    int lenf = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    return lenf;
+}
+
+
 
 int main()
 {          
@@ -11,8 +21,19 @@ int main()
     const int WINDOW_HEIGHT = 500;
 
     int count = 0;
-    string visibleText = new char[len + 1];
-    string buf = buffer;    
+    int quantityN = 0;
+
+    FILE * code = fopen("hackercode.py", "r");
+
+    int len = fileLen(code);   
+    char* buffer = new char[len + 1];
+
+    fread(buffer, sizeof(char), len, code);
+    fclose(code);
+
+    String visibleText = new char[len + 1];
+
+    String buf = buffer;    
 
     sf::Font font;    
     font.loadFromFile("arial.ttf");
@@ -23,7 +44,7 @@ int main()
     visText.setFillColor(sf::Color::Green);
 
 
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "HackerTyper");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Hackertyper");
 
     while (window.isOpen())
     {
@@ -31,29 +52,36 @@ int main()
         while (window.pollEvent(evnt))
         {
             if (evnt.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+			if (evnt.type == sf::Event::KeyReleased)
+			{  
+            char* newSymbol;
+            newSymbol[0] = buffer[count];
+            newSymbol[1] = buffer[count + 1];
+
+            std::cout<<newSymbol<<std::endl;
+
+            if (visibleText.countChar('\n'))
             {
-                 window.close();
+
             }
-            if (evnt.type == sf::Event::KeyReleased)
-            {  
-                  char* newSymbol;
-                  newSymbol[0] = buffer[count];
-                  newSymbol[1] = buffer[count + 1];
+            count += 2;
 
-                  std::cout<<newSymbol<<std::endl;
+            String newText = newSymbol; 
 
-                  if (visibleText.countChar('\n'))
-                  {
-                  count += 2;
-                  string newText = newSymbol; 
-                  visibleText = visibleText + newText;
-                  }  
-              }
+            visibleText = visibleText + newText;
+            }  
+        }
             visText.setString(visibleText.str);
             
             window.clear();
             window.draw(visText);
             window.display();    
+        
+
+
     }
     return 0;
 }
